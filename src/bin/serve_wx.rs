0a6f873f7eaae1098087d7e11797156c34443a23
KujouRinka <kujourinka@gmail.com>
2022-11-19 15:12:00 +0800

    add test data

--- src/bin/serve_wx.rs
+++ src/bin/serve_wx.rs
@@ -34,6 +34,39 @@ async fn main() {
   ).await.unwrap();
   info!("Create Sql connection pool OK");
 
+  // random init some data
+  pool.add_university("tsinghua", "清华大学").await; // 0
+  pool.add_university("peking", "北京大学").await;   // 1
+  pool.add_university("muc", "中央民族大学").await;  // 2
+  pool.add_university("tongji", "同济大学").await;  // 3
+  pool.add_university("sichuan", "四川大学").await; // 4
+  pool.add_university("hunan", "湖南大学").await;   // 5
+  pool.add_university("tianjin", "天津大学").await; // 6
+  pool.add_university("beili", "北京理工大学").await; // 7
+  pool.add_university("harbin", "哈尔滨工业大学").await; // 8
+
+  pool.add_department(3, "cs", "计算机科学与技术").await; // 0
+  pool.add_department(3, "ee", "电子工程").await; // 1
+  pool.add_department(3, "me", "机械工程").await; // 2
+  pool.add_department(3, "ce", "土木工程").await; // 3
+  pool.add_department(3, "se", "软件工程").await; // 4
+  pool.add_department(3, "ie", "信息工程").await; // 5
+  pool.add_department(3, "ae", "自动化工程").await; // 6
+  pool.add_department(3, "pe", "物理工程").await; // 7
+  pool.add_department(3, "ch", "化学工程").await; // 8
+  pool.add_department(3, "me", "材料工程").await; // 9
+  pool.add_department(3, "me", "数学与应用数学").await; // 10
+  pool.add_department(3, "me", "统计学").await; // 11
+  pool.add_department(3, "me", "经济学").await; // 12
+  pool.add_department(1, "se", "软件工程").await; // 4
+  pool.add_department(1, "ee", "电子工程").await; // 5
+  pool.add_department(1, "me", "机械工程").await; // 6
+  pool.add_department(1, "ce", "土木工程").await; // 7
+  pool.add_department(2, "cs", "计算机科学与技术").await; // 8
+  pool.add_department(2, "ee", "电子工程").await; // 9
+  pool.add_department(2, "me", "机械工程").await; // 10
+  pool.add_department(2, "ce", "土木工程").await; // 11
+
   let ctx = Context::new(pool, Arc::new(options));
   println!("{:?}", ctx);
 
